4f31be0f07f38bcc5bb3384980abb1c4d4a47129
Amal Qurany <mal.qurany@gmail.com>
2018-04-09 06:06:36 +0700

    add net-speeech-api and speechutils submodule

--- app/src/main/java/org/tangaya/quranasrclient/MainActivity.java
+++ app/src/main/java/org/tangaya/quranasrclient/MainActivity.java
@@ -2,6 +2,8 @@ package org.tangaya.quranasrclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void recordButtonClickAction(View view) {
+
+        Toast.makeText(this, "record button clicked", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
